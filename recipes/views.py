584e252dbc483f5d96b4f7b765da600fe7e260db
carloscfas <Carloasccasd15574@outlook.com>
2024-02-28 11:53:40 -0300

    created function the used a jinja template

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
+from utills.recipes.factory import make_recipe
 
 # Create your views here.
 
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Carlos Felipe',
+        'recipes': [make_recipe() for _ in range(10)],
     })
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Carlos Felipe',
+        'recipe': make_recipe(),
     })
