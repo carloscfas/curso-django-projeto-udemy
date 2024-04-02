b6bdcdb4e601cf4acea49f2d7d61997ea6b07180
carloscfas <Carloasccasd15574@outlook.com>
2024-04-02 11:49:41 -0300

    Criando campos no models do django e criando lógicas com as views

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-Cursando esse grande curso do Ótavio Miranda!!
+Para começar a utilizar devemos criar o ambiente virtual:
+
+    $ python -m venv venv
+
+Agora ative o ambiente virtual:
+
+    $ source venv/scripts/activate
+
+Instale as bibliotecas necessarias:
+
+    $ pip install -r requirements.txt
+
+Para Rodar o project:
+
+    $ python3 manage.py runserver
--- recipes/models.py
+++ recipes/models.py
@@ -6,10 +6,13 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
-    slug =  models.SlugField()
+    slug =  models.SlugField() 
     preparation_time = models.IntegerField()
     preparation_time_unity = models.CharField(max_length=65)
     servings_time = models.IntegerField()
@@ -19,6 +22,9 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, default=None,)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
